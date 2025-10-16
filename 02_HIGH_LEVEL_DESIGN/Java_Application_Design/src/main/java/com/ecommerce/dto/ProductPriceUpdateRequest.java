dd35acf6b9d524a8623948d415d96992e3c600cb
Ajay kumar Pondugala <aponduga@cisco.com>
2025-10-16 23:24:31 +0530

    Reorder import statements

--- 02_HIGH_LEVEL_DESIGN/Java_Application_Design/src/main/java/com/ecommerce/dto/ProductPriceUpdateRequest.java
+++ 02_HIGH_LEVEL_DESIGN/Java_Application_Design/src/main/java/com/ecommerce/dto/ProductPriceUpdateRequest.java
@@ -1,7 +1,9 @@
 package com.ecommerce.dto;
 
-import jakarta.validation.constraints.*;
 import java.math.BigDecimal;
+
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 
 public record ProductPriceUpdateRequest(
         @NotNull @DecimalMin(value = "0.01", inclusive = true) BigDecimal newPrice
